1259fe12d9630e7853646fe306376f5e3be211c1
xido <xidoweb@gmail.com>
2019-10-04 10:37:59 +0900

    variable 01

---
+++ d_js_basic/js/src/data_type_01.js
@@ -0,0 +1,27 @@
+// data_type_01.js
+
+/*
+// 변수의 기초 개념이해하기
+
+var int;
+console.log(int);
+
+var 댕댕이;
+console.log(댕댕이);
+
+댕댕이 = "멍멍이";
+console.log(댕댕이);
+
+console.log(멍댕이);
+
+* 결과값을 참고하여 
+* 값이 undefined라면, 변수명은 선언했지만 값은 지정하지 않은 형태
+* 값이 not defined라면, 변수를 생성조차 하지 않은것
+*/
+// console.log(멍댕이);
+
+var rel = "10:01";
+
+console.log(rel);
+
+// 스크립트기반 또는 언어라고 불리우는 프로그래밍은 하나의 에러가나면 이후의 내용은 동작하지 않는다.
