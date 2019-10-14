12d3e672d39e66c5dbad286bcca792f2c325ecae
xido <xidoweb@gmail.com>
2019-10-14 18:32:47 +0900

    jquery selector

--- d_js_basic/js/src/jquery/jq_02.js
+++ d_js_basic/js/src/jquery/jq_02.js
@@ -13,22 +13,27 @@ document.querySelector('p');
 $('p')
 */
 
-$('.box p')
-$('.box').find('p')
+/*
+$('.box p').css({"padding":"0.5rem", "backgroundColor":"#fa0"});
+$('.box').find('p').css({"color":"#f03", "textShadow":"0.2rem 0.2rem 0.2rem rgba(0,0,0,0.7)"});
 
-$('.box > p')
-$('.box').children('p')
+$('.box > p').css({"borderBottom":"2px solid #111"});
+$('.box').children('p').css({"transform":"rotate(45deg)"});
+*/
 
-$('dl > dt + dd')
-$('dl').children('dt').next('dd')
+$('dl > dt + dd').css({"backgroundColor":"#acc"});
+$('dl').children('dt').next('dd').css({"fontSize":"2rem"});
 
-$('dl > dt ~ dd')
-$('dl').children('dt').nextAll('dd')
+$('dl > dt ~ dd').css({"marginLeft":"2rem"});
+$('dl').children('dt').nextAll('dd').css({"color":"#f03"});
 
-$('ul > li:nth-child(1)')
-$('ul').children('li:nth(0)')
-$('ul').children('li').eq(0)
+$('ul > li:nth-child(1)').css({"backgroundColor":"#ffa"});
+$('ul').children('li:nth(0)').css({"color":"#077"});
+$('ul').children('li:nth(-1)').css({"color":"#f77"});
+$('ul').children('li').eq(0).css({"borderBottom" : "2px solid #333"});
+$('ul').children('li').eq(-1).css({"borderBottom" : "2px solid #f05"});
 
+$('ul').find('li:eq(-2)').siblings().css({"borderLeft": "3px solid #f06"});
 
 // 자식 : children
 // 자손 : find
