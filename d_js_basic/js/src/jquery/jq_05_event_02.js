aed757cfd150956794a39e4300b98df26ed77ea0
xido <xidoweb@gmail.com>
2019-10-16 10:32:32 +0900

    focus 2

--- d_js_basic/js/src/jquery/jq_05_event_02.js
+++ d_js_basic/js/src/jquery/jq_05_event_02.js
@@ -4,18 +4,26 @@
   let menu = $('.menu');
   let menuUl = menu.children('ul');
   let menuLi = menuUl.children('Li');
+  let menuLink = menuLi.children('a');
 
+  let mLiBg = menuLi.css('backgroundColor');
+  let mLiBdr = menuLi.css('borderRadius');
+  let mLinkColor = menuLink.css('color');
+  let mLinkWeight = menuLink.css('fontWeight');
+  menuLink.css({'outline':0});
+  console.log(mLiBg ,mLiBdr ,mLinkColor ,mLinkWeight);
 // focus:초점이 잡힌상태(a, button, form내부의 요소)
-  menuLi.children('a').on('focus', function(){
+  menuLink.on('focus', function(){
     let mythis = $(this);
-    mythis.parent('li').css({'backgroundColor':'#0af', 
-                              'borderRadius':'0.5rem'});
-    mythis.css({'color':'#fff', 'fontWeight':'bold', 'outline':'0'});
+    mythis.parent('li').css({'backgroundColor':'#0af','borderRadius':'0.5rem'});
+    mythis.css({'color':'#fff', 'fontWeight':'bold'});
   });
 
 // blur: 초점이 잡힌요소가 풀리는 상황
-  menuLi.children('a').on('focus', function(){
-    
+  menuLink.on('blur', function(){
+    let mythis = $(this); 
+    mythis.parent('li').css({ 'backgroundColor': mLiBg, 'borderRadius': mLiBdr});
+    mythis.css({ 'color': mLinkColor, 'fontWeight': mLinkWeight});
   });
 
 
