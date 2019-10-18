327e94f68cde5f9ad812b10947eb3d3290127c7f
xido <xidoweb@gmail.com>
2019-10-18 16:05:09 +0900

    spigen step3 side_gnb_area css set

--- e_example/js/make_spigen_main_pc.js
+++ e_example/js/make_spigen_main_pc.js
@@ -1,4 +1,15 @@
 // make_spigen_main_pc
 (function($){
-  
+  // 1. #gnb영역내부에 있는 ul의 내용을 .side_gnb_area에 복사해서 붙여넣기
+  // clone() 메서드를 사용
+
+
+  const gnb = $('#gnb');
+  const sideGnbArea = $('.side_gnb_area');
+
+  let gnbContents = gnb.contents().clone();
+  // console.log(gnbContents);
+  sideGnbArea.append(gnbContents);
+
+
 })(jQuery);
