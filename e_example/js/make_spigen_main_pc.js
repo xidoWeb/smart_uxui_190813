5ecd6fac8c7ea17e4c6d68df020c9ce4ca0c4109
xido <xidoweb@gmail.com>
2019-10-18 17:46:23 +0900

    gnb addClass step_01

--- e_example/js/make_spigen_main_pc.js
+++ e_example/js/make_spigen_main_pc.js
@@ -10,6 +10,56 @@
   let gnbContents = gnb.contents().clone();
   // console.log(gnbContents);
   sideGnbArea.append(gnbContents);
+// ---------------------------------------------
+// 버튼클릭시 .side_gnb 나타나게 만들기/사라지게 만들기
+
+  const openGnbBtn   = $('.gnb_btn > button');
+  const closeGnbBtn  = $('.close_gnb_btn > button');
+  const sideGnb      = $('.side_gnb');
+  const gnbDl        = gnb.find('dl');
+  const gnbDd        = gnb.find('dd');
+  const gnbDt        = gnb.find('dt');
+  const gnbTitleLink = gnbDt.children('a');
+  const gnbListLink  = gnbDd.children('a');
+  
+  let time           = 600;
+
+  openGnbBtn.on('click', function(e){
+    e.preventDefault();
+    sideGnb.fadeIn(time/2);
+  });
+  closeGnbBtn.on('click', function (e) {
+    e.preventDefault();
+    sideGnb.fadeOut(time);
+  });
+
+
+// #gnb에 마우스 올렸을 경우 dd를 나오게 만들기
+
+
+
+gnbTitleLink.on('mouseenter', function(){
+  $(this).addClass('action');
+  gnbDd.stop().slideDown();
+});
+
+gnb.on('mouseleave', function(){
+  gnbTitleLink.removeClass('action');
+  gnbDd.stop().slideUp();
+});
+
+// #gnb에 dt에 focus 처리되면 dd가 나타나게 만들기
+// a, button, form(input, textarea, select)
+
+
+  gnbTitleLink.on('focus', function(){
+    gnbDd.stop().slideDown();
+  });
+
+  gnbListLink.eq(-1).on('blur', function(){
+    gnbDd.stop().slideUp();
+  });
+
 
 
 })(jQuery);
