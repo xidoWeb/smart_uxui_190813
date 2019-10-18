4f9e6fc7d5be157f90dc29775f6a29a8a7581070
xido <xidoweb@gmail.com>
2019-10-18 15:23:08 +0900

    accordion test

--- d_js_basic/js/src/jquery/gnb_01.js
+++ d_js_basic/js/src/jquery/gnb_01.js
@@ -17,34 +17,51 @@
    const partList = titleLink.next('ul');
    const subLink = partList.find('a');
    let timed = 500;
+
+   // 3번 
+  titleLink.on('mouseenter focus', function(){
+    let ulSlide = $(this).next(partList);
+    ulSlide.parent().siblings().find(partList).slideUp();
+    ulSlide.slideDown();
+
+    ulSlide.find('a').eq(-1).on('blur', function(){
+      ulSlide.slideUp();
+    });
+  });
+  gnb.on('mouseleave', function(){
+    partList.slideUp();
+  });
+
+
 // =====================================================
-// 1번기능 수행하기
-   gnb.css({'backgroundColor':'transparent'});
-   gnbArea.css({'height':'auto','backgroundColor':'#fff', 
-                'boxShadow':'0.2rem 0.2rem 0.2rem rgba(0,0,0,0.3)'});
+/*
+  // 1번기능 수행하기
+    gnb.css({'backgroundColor':'transparent'});
+    gnbArea.css({'height':'auto','backgroundColor':'#fff', 
+                  'boxShadow':'0.2rem 0.2rem 0.2rem rgba(0,0,0,0.3)'});
 
-   // 포커스 잡히면 보이게하기
-   titleLink.on('focus', function(e){
-     e.preventDefault();
-     partList.stop().slideDown(timed);
-   });
-   // 마지막요소 블러처리되면 사라지게
-   subLink.eq(-1).on('blur', function(){
-     partList.stop().slideUp(timed);
-   });
-   // gnbArea에 마우스 올렸을경우 나타나게하기
-   gnbArea.on('mouseenter', function(){
-     partList.stop().slideDown(timed);
-   });
-   gnb.on('mouseleave', function(){
-     partList.stop().slideUp(timed);
-   });
+    // 포커스 잡히면 보이게하기
+    titleLink.on('focus', function(e){
+      e.preventDefault();
+      partList.stop().slideDown(timed);
+    });
+    // 마지막요소 블러처리되면 사라지게
+    subLink.eq(-1).on('blur', function(){
+      partList.stop().slideUp(timed);
+    });
+    // gnbArea에 마우스 올렸을경우 나타나게하기
+    gnbArea.on('mouseenter', function(){
+      partList.stop().slideDown(timed);
+    });
+    gnb.on('mouseleave', function(){
+      partList.stop().slideUp(timed);
+    });
 
-// ==========================================
-// 2 번기능 수행하기
-  gnbArea.css({'height': '100%'});
-  // 이외의 기능은 1번기능과 동일
-
+  // ==========================================
+  // 2 번기능 수행하기
+    gnbArea.css({'height': '100%'});
+    // 이외의 기능은 1번기능과 동일
+*/
 // ==========================================
 // 3번기능 수행하기
 
