b274e20da442f687bde2869060b8284a45bda9fe
xido Lee <erivery2@gmail.com>
2019-10-24 12:34:12 +0900

    data link

--- e_example/js/src/spigen_temp_pc/make_spigen_header_pc.js
+++ e_example/js/src/spigen_temp_pc/make_spigen_header_pc.js
@@ -6,6 +6,91 @@
 
   const gnb = $('#gnb');
   const sideGnbArea = $('.side_gnb_area');
+// ============================================
+//TODO: header영역에 해당하는 내용을 만들기
+
+const gnbMenu = [
+  {title:'about',titleLink:'title_link_0',
+   // sub:['Who we are','What we do','Our locations']}, 
+   sub:[
+      {subName:'Who we are'   , subLink:'sub_01_0'},
+      {subName:'What we do'   , subLink:'sub_01_1'},
+      {subName:'Our locations', subLink:'sub_01_2'}
+    ]},
+　{title:'careers',titleLink:'title_link_1',
+   sub:[
+		  {subName:'careers', subLink:'sub_02_0'},
+      {subName:'HR Blog', subLink:'sub_02_1'},
+      {subName:'apply'  , subLink:'sub_02_2'}
+    ]},
+　{title:'media',titleLink:'title_link_2',
+   sub:[
+      {subName:'media', subLink:'sub_03_0'}
+    ]},
+　{title:'IR',titleLink:'title_link_3',
+   sub:[
+     {subName:'investors', subLink:'sub_04_0'},
+     {subName:'IR archive', subLink:'sub_04_1'},
+		 {subName:'IR meeting', subLink:'sub_04_2'}
+		]}
+]; //  gnbMenu 
+
+// console.log(gnbMenu);
+// ===================================================
+// gnbMenu[0].title
+// console.log(gnbMenu[0].sub[0]);
+/* for(let i=0; i < gnbMenu[0].sub.length; i++){
+  console.log(gnbMenu[0].sub[i]);
+}
+for(let i=0; i<gnbMenu[1].sub.length; i++){
+  console.log(gnbMenu[1].sub[i]);
+}
+for(let i=0; i<gnbMenu[2].sub.length; i++){
+  console.log(gnbMenu[2].sub[i]);
+}
+for(let i=0; i<gnbMenu[3].sub.length; i++){
+  console.log(gnbMenu[3].sub[i]);
+} */
+/*
+  for(let i=0; i<gnbMenu.length; i++){
+    console.log(gnbMenu[i].title);
+    // ---------------------
+    for(let j=0; j < gnbMenu[i].sub.length; j++){
+      console.log(gnbMenu[i].sub[j])
+    }
+    // ---------------------
+  }
+*/
+// ===================================================
+
+const menuLen = gnbMenu.length;
+gnb.append('<ul></ul>'); // gnb영역에 ul 생성
+const gnbUl = gnb.children('ul');
+
+for(let i = 0; i < menuLen; i++){
+  // console.log(gnbMenu[i]);
+  // gnb.append(gnbMenu[i]);
+  gnbUl.append('<li><dl><dt><a href=""></a></dt><dd></dd></dl></li>');
+  let gnbLi = gnbUl.children('li').eq(i);
+	let gnbLiLink = gnbLi.find('dt').children('a');
+	gnbLiLink.attr({'href' : gnbMenu[i].titleLink});  // 링크 속성변경
+	gnbLiLink.text(gnbMenu[i].title);
+
+  let subLen = gnbMenu[i].sub.length;
+  // console.log(subLen);
+  for(let j=0; j <subLen; j++){
+    let gnbDd = gnbLi.find('dd');
+    gnbDd.append('<a href=""></a>');
+    let gnbMyLink = gnbDd.children('a').eq(j);
+		gnbMyLink.text(gnbMenu[i].sub[j].subName);
+		gnbMyLink.attr({'href': gnbMenu[i].sub[j].subLink});
+  }
+} // for(i < menuLen)
+
+
+
+
+// ============================================
 
   let gnbContents = gnb.contents().clone();
   // console.log(gnbContents);
@@ -65,10 +150,6 @@ const removeAction = function () {
   gnbDd.stop().slideUp();
 };
 
-/*
-  gnbDl.on('mouseenter', addAction);
-  gnbDl.on('mouseleave', removeAction);
-*/
   gnbDl.on({ 'mouseenter': addAction, 'mouseleave': removeAction});
 
 // #gnb에 dt에 focus 처리되면 dd가 나타나게 만들기
