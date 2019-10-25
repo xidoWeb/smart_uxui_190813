7d5759af7d3076e9a5d8e0f3c15bcb28c199086b
xido Lee <erivery2@gmail.com>
2019-10-25 18:04:22 +0900

    banner jquery test_01

--- f_new_example/public/js/src/main.js
+++ f_new_example/public/js/src/main.js
@@ -2,17 +2,19 @@
 (function($){
   const body = $('body'); // body
   const wrap = $('#wrap'); // #wrap
-  wrap.append('<section id="viewBox"></section>'); // #viewBox생성
-  const viewBox = $('#viewBox');
   // script 내용 간편작성함수 생성
   const JsLink = (temp) => {
     return body.append(`<script src="../js/src/temp/${temp}.js"></script>`);
   };
 
+  // 광고 영역 생성 ======================================================
+  wrap.append('<section id="viewBox"></section>'); // #viewBox생성
+  const viewBox = $('#viewBox');
+
   viewBox.load('./temp/slide_01.html', () => {
     JsLink('slide_01');
   });
-
+// ======================================================
 
 
 })(jQuery);
--- f_new_example/public/js/src/temp/slide_01.js
+++ f_new_example/public/js/src/temp/slide_01.js
@@ -1,1 +1,33 @@
 // slide_01.js
+(function($){
+  // .view_indicator 내부의 li(a)를 선택하면,
+  // #viewBox 내부의 .guide 박스가 이동하게 만들어라.
+
+  // index() 순서 가져오는 것, eq() 순서를 주는것
+  // margin-left: -100%, -200%  || position:relative; left:-100%, -200% ...
+
+  const viewBox   = $('#viewBox');
+  const indicator = viewBox.find('.view_indicator');
+  const indiLi    = indicator.find('li');
+  const indiLink  = indiLi.children('a');
+  const slideForm = viewBox.find('.slide_form');
+  const guideBox  = viewBox.find('.guide');
+
+  slideForm.css({'overflow':'hidden'});
+  indicator.css({'zIndex': 500});  
+  guideBox.css({'position':'relative','top':0,'left':0});
+
+  indiLink.on('click focus', function(evt){
+    evt.preventDefault();
+    // let parLi = $(this).parent('li');
+    let i = $(this).parent('li').index();
+    console.log(i);
+
+    indiLink.removeClass('action');
+    $(this).addClass('action');
+    guideBox.stop().animate({'left':-100 * i + '%'});
+
+  });
+
+
+})(jQuery);
