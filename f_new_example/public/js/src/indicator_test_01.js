1eaf98f29d7f2cb1d936d7bcfb35d68a5317292c
xido Lee <erivery2@gmail.com>
2019-11-05 14:35:10 +0900

    slide indicator test 02

--- f_new_example/public/js/src/indicator_test_01.js
+++ f_new_example/public/js/src/indicator_test_01.js
@@ -1,6 +1,30 @@
 // indicator_test_01.js
 (function($){
+  const miniProduct = $('#miniProduct');
+  const indicator   = miniProduct.children('.indicator');
+  const indiLi      = indicator.children('li');
+  const indiLink    = indiLi.children('a');
   
-
+  const adverBanner = miniProduct.find('.product').children('ul');
+  const adverNth    = adverBanner.children('li');
+  
+  indiLi.eq(0).addClass('active');
+  // -----------------------------------------------
+  indiLink.on('focus', function(e){
+    e.preventDefault();
+    let i = $(this).parent().index();
+    adverBanner.animate({marginLeft: (-100 * i) + '%' });
+    
+    indiLi.eq(i).addClass('active');
+    indiLi.eq(i).siblings('li').removeClass('active');
+    // $(this).parent().addClass('active');
+    // $(this).parent().siblings('li').addClass('active');
+    
+    $(this).on('click', function(e){
+      e.preventDefault();
+      adverNth.eq(i).children('a').focus();
+    });
+    
+  });
 
 })(jQuery);
