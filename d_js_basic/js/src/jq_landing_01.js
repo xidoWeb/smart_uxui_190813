b0d5c869c8c3e3bf4570b005759186de195fa63a
xido Lee <erivery2@gmail.com>
2019-11-11 16:23:22 +0900

    img place

--- d_js_basic/js/src/jq_landing_01.js
+++ d_js_basic/js/src/jq_landing_01.js
@@ -18,7 +18,13 @@
   // con_02의 위치에서 브라우저의 1/2 높이만큼 전에서 구동처리
   let con_02_offset = con_02.offset().top;
   let myCon02_offset = con_02_offset - (winH/2);
+
+  // con_03의 위치파악
+  let con_03_offset = con_03.offset().top;
+  let myCon03_offset = con_03_offset - (winH/2);
   
+  
+  //----------------------------------------------
   win.on('scroll', function(){
    let winScroll = win.scrollTop();
    // headBox 기능
@@ -51,8 +57,24 @@
     }else{
       $('.con_02').removeClass('action');
    }
+   //--------------------------------------------
+   // scroll 처리시 03이미지가 왼쪽에서 부터 오른쪽으로 이동하게...
+   let n = 0;
+   if(winScroll > myCon03_offset){
+     let myLeft = winScroll - myCon03_offset;     
+     let myn = Math.round(myLeft % 21);
+     if(myn < 10){n = '00' + myn;
+     }else if(myn < 100){n = '0' + myn;
+     }else{n = myn;}
+    
+     con_03.find('.mv_bird')
+           .css({top:myLeft + 'px', left:myLeft + 'px',
+    backgroundImage:'url(../../img/bird/bird_'+ n +'.png)'});
+   }
    
   });
-  
+  // Math.random(), Math.round(), Math.ceil(),Math.floor()
+  // Math.abs(-4.9);
+ 
   
 })(jQuery);
