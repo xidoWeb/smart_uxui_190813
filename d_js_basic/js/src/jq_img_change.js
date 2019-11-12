912ab04bf11ccae3ef8d102e80c8c1b40e890c7d
xido Lee <erivery2@gmail.com>
2019-11-12 16:15:59 +0900

    img mv

--- d_js_basic/js/src/jq_img_change.js
+++ d_js_basic/js/src/jq_img_change.js
@@ -1,11 +1,12 @@
 (function($){
-  const wrap = $('#wrap');
-  const myForm = wrap.find('.mouse_form');
+  const wrap       = $('#wrap');
+  const myForm     = wrap.find('.mouse_form');
   const myLocation = wrap.find('.mouse_location');
-  const myMvBtn = myLocation.children('button');
+  const myMvBtn    = wrap.find('.mv_btn');
+  const myBtn      = myMvBtn.find('button');
 
-  let url = '../../img/gear/';
-  let j=0;
+  let url          = '../../img/gear/';
+  let j            =0;
   for(let i=0; i<100; i++){  
     if(i < 9){ j = '00' + (i+1); 
     }else if(i < 99){ j = '0' + (i+1);
@@ -25,5 +26,42 @@
     myImg.eq(myi).siblings('img').hide();
     myImg.eq(myi).show();
   }); */
+  let thisWidth = myForm.outerWidth();
+  const ChangeImg = function(){
+    let myi = Math.floor( mv / thisWidth * 100 );    
+    myImg.eq(myi).siblings('img').hide();
+    myImg.eq(myi).show();
+  };
 
+  let is_draggable = false;
+  let mv=0;
+  $('head').append('<style class="samStyle"></style>');
+  const samStyle = $('.samStyle');
+  const Mvbtn = function(){
+    if(is_draggable){
+      // is_draggable = true;
+      samStyle.text(`.mouse_location:after{left:${mv-10}px}`);
+      // console.log(mv);
+      ChangeImg();
+    }
+  }// Mvbtn();
+  myLocation.on('mousemove', function(e){
+    mv = e.originalEvent.offsetX;
+    if(mv < 0){mv = 0}else if(mv > thisWidth){ mv = thisWidth}
+    Mvbtn();
+  });  
+  
+  myLocation.on('mousedown', function(e){
+    e.preventDefault();
+    mv = e.originalEvent.offsetX;    
+    is_draggable = true;
+    Mvbtn();
+  });
+  
+  myLocation.on('mouseup', function(e){
+    e.preventDefault();
+    is_draggable = false;
+  });
+
+  
 })(jQuery);
