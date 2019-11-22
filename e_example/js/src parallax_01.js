334e2652acaa3f06443788c8beaf7faabb96cf51
xido <xidoweb@gmail.com>
2019-11-22 12:14:10 +0900

    coffee parallax img

--- e_example/js/src parallax_01.js
+++ e_example/js/src parallax_01.js
@@ -11,16 +11,13 @@
   let winH = win.outerHeight();
   
   win.on('scroll', function(){
-    let scroll = win.scrollTop()/4 *3;
+    let scroll = win.scrollTop();
     // console.log(scroll);
-    $(par[0]).css({backgroundPositionY: -scroll/8 });
-    $(par[1]).css({backgroundPositionY: -scroll/7 });
-    $(par[2]).css({backgroundPositionY: -scroll/6 });
-    $(par[3]).css({backgroundPositionY: -scroll/5 });
-    $(par[4]).css({backgroundPositionY: -scroll/4 });
-    $(par[5]).css({backgroundPositionY: -scroll/3 });
-    $(par[6]).css({backgroundPositionY: -scroll/2 });
-    $(par[7]).css({backgroundPositionY: -scroll });
-    $(par[8]).css({backgroundPositionY: -scroll });
+    for(let j=0; j < par.length; j++){
+      $(par[j]).css({top: -scroll / (par.length - j) });
+    }
   }); 
+  // ------------------------------------------------------
+  
+  
 })(jQuery);
