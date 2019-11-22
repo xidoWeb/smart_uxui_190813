836cac15e53f5b2139804de838c5cd8f858c7b15
xido <xidoweb@gmail.com>
2019-11-22 16:01:43 +0900

    parallax

--- e_example/js/src parallax_01.js
+++ e_example/js/src parallax_01.js
@@ -10,14 +10,37 @@
   
   let winH = win.outerHeight();
   
-  win.on('scroll', function(){
+  win.on('scroll', function(e){
+    e.preventDefault();
     let scroll = win.scrollTop();
     // console.log(scroll);
     for(let j=0; j < par.length; j++){
-      $(par[j]).css({top: -scroll / (par.length - j) });
+      $(par[j]).css({top: -scroll / (par.length - j -1) });
     }
   }); 
   // ------------------------------------------------------
   
+  const conBox = $('#conBox');
+  const conList = conBox.find('li');
+  const liOffset = [];
+  
+  for(let i=0; i<conList.length; i++){
+    //liOffset.push(conList.eq(i).offset().top);
+    liOffset[i] = conList.eq(i).offset().top;
+  }
+  console.log(liOffset);
+  
+  
+  win.on('scroll', function(e){
+    e.preventDefault();
+    let scroll = win.scrollTop();
+    let scRel  = [];
+    for(let i=0; i<conList.length; i++){
+     scRel[i] = liOffset[i] - scroll - (winH/4);      
+     conList.eq(i).find('span')
+            .css({transform:'translateY(-'+ -scRel[i]/4 +'px)'});
+    }    
+    console.log(scRel[0])
+  });
   
 })(jQuery);
