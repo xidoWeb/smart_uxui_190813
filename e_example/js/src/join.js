c1f550682fd085424c43d19344f570916e53c659
xido <xidoweb@gmail.com>
2019-11-25 11:06:34 +0900

    join js step_02

--- e_example/js/src/join.js
+++ e_example/js/src/join.js
@@ -7,9 +7,41 @@
     if(thisVal == ""){
       $(this).closest('li').addClass('error');
     }
+  });
   // -----------------------------------------------
   // pw 비교해서 일치여부 
+  const userPw   = $('#userPw');
+  const userPwRe = $('#userPwre');
+  let beforePwVal, afterPwVal;
+  userPw.on('keyup', function(){ 
+   beforePwVal = userPw.val();
+   let thisVal = $(this).val();
+   let thisLi = $(this).closest('li');
+   if(thisVal == ""){
+     thisLi.find('.narr').text('비밀번호를 입력하세요.');
+     thisLi.addClass('error');
+    }if(thisVal.length <= 5){
+      thisLi.find('.narr').text('비밀번호는 6~15입니다.');
+      thisLi.addClass('error');
+   }else{
+      thisLi.removeClass('error');
+   }
+  });  
+  
+  //비밀번호 확인
+  userPwRe.on('keyup', function(){ 
+   afterPwVal = userPwRe.val();
+    if(beforePwVal !== afterPwVal){
+      $(this).closest('li').addClass('error');
+    }else{
+      $(this).closest('li').removeClass('error');
+    }
+  });
   
     
-  });
+   
+ 
+ 
+ 
+ 
 })(jQuery);
