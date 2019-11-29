07db3bd4ea6a8b1879c8dfd0de9803f426f01920
xido <xidoweb@gmail.com>
2019-11-29 15:34:57 +0900

    apple modify1

--- e_example/js/src/scroll_image.js
+++ e_example/js/src/scroll_image.js
@@ -1,33 +1,11 @@
 (function($){
-  const win = $(window);
-  const wrap = $('#wrap');
-  
+  const win = $(window); 
   const viewBox = $('#viewBox');
   
-  // viewBox.css({position:'fixed', top:0, backgroundColor:'#fff'});
-  viewBox.find('.fix_img').children('img').css({position:'sticky', top:'40px', backgroundColor:'#0ff'});
+  viewBox.find('.fix_img').wrap('<div class="outer_wrap"></div>');
+  $('.outer_wrap').css({height:'2000px'});
   
-  /* const subConBox = $('#sub_conBox');  
-  const videoConBox = $('#video_conBox');
-  const graphConBox = $('#graph_conBox');
-  const osConBox = $('#os_conBox');
-
-  subConBox.css({ position: 'fixed', bottom: '100%', zIndex:4});
-  videoConBox.css({ position: 'fixed', bottom: '100%', zIndex:3});
-  graphConBox.css({ position: 'fixed', bottom: '100%', zIndex:2});
-  osConBox.css({ position: 'fixed', bottom: '100%', zIndex:1});
-
-  let totalH = viewBox.outerHeight(true) +
-               subConBox.outerHeight(true) +
-               videoConBox.outerHeight(true) +
-               graphConBox.outerHeight(true) +
-               osConBox.outerHeight(true);
-
-  wrap.css({ height: totalH });
-
-  viewBox.css({position:'fixed', top:0, backgroundColor:'#fff'}); */
-  
-  viewBox.find('.title').css({position:'relative', top:0});
+  viewBox.find('.fix_img').css({position:'sticky', top:'50px'});
   
   // 이미지 담기
   const viewFix = viewBox.find('.fix_img');
@@ -47,27 +25,18 @@
   let imgH = viewBox.find('.title').outerHeight() +
              viewFix.children('img').eq(0).outerHeight();
   viewFix.next('p').css({marginTop: imgH + 'px'});
-// =======
-  /* 
-  let winH  = win.outerHeight();
-  let viewH = viewBox.outerHeight();
-  let viewOfH =  viewH + 122;
-  console.log(viewOfH); */
-// ---------------------------------------------------------
-
 
 
   //-- 스크롤기능
-
   win.on('mousewheel',function(){
     let thisS = $(this).scrollTop();
     
     // 글씨 투명해지게 만들기
     let op = 1 - ( thisS *  0.001);
     if(op < 0){op = 0}    
-// <<<<<<< 
-    // viewBox.find('.title').css({opacity:op});    
-    viewBox.find('.title').css({top:(-thisS / 3)+'px', opacity:op});    
+    
+    viewBox.find('.title').css({top:(-thisS / 7)+'px', opacity:op});    
+    
     // -----------------------------------------------------------
     // 이미지 교체하기
     let imgI = parseInt(thisS / 2000 * 121);
@@ -75,29 +44,7 @@
     // console.log(imgI);
     viewFix.children('img').eq(imgI).siblings().hide();
     viewFix.children('img').eq(imgI).show();   
-// =======
-    //----------------------------------------
-    /* 
-    viewBox.find('.title').css({top:(-thisS / 3)+'px', opacity:op});    
-    // -----------------------------------------------------------
-    let indI = Math.ceil(thisS / (viewH+122) * 100);
-    let indIP = parseInt(indI);
-    viewFix.children('img').eq(indIP).show();
-    viewFix.children('img').eq(indIP).siblings().hide();
 
-    let viewMv = 0;
-    if (thisS >= viewOfH){
-      viewMv = (thisS - viewOfH) / 2;
-      viewBox.css({top:-viewMv + 'px'});
-    }
-    console.log(thisS, viewOfH);
-
-    // -----------------------------------------------------
-    let viewEndOffset =  viewBox.offset().top - winH;
-    if (thisS >= viewEndOffset){
-      subConBox.css({ top: -viewMv+ 'px'});
-    }
- */
   });
   
 })(jQuery);
