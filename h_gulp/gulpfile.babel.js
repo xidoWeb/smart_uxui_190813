f30d51d5de5b2d1d37b308fb0f3f6fef06b89571
xido <xidoweb@gmail.com>
2019-12-20 05:22:51 +0900

    gulp data

--- h_gulp/gulpfile.babel.js
+++ h_gulp/gulpfile.babel.js
@@ -1,14 +1,13 @@
 //모듈 불러오기
-import gulp        from  "gulp";
-import corejs      from  "core-js";
-import mkdir       from  "mk-dirs";    // 폴더생성
-import writeFile   from  "write";      // 파일생성
-import filedel     from  "del";
-import gulpCached  from  "gulp-cached";
-import gulpWatch   from  "gulp-watch";
-import gulpScss    from  "gulp-sass";
-import babel       from  "gulp-babel";
-import gulpSync    from  "browser-sync";
+import gulp          from  "gulp";          // gulp 기본
+import corejs        from  "core-js";       // async 동작처리
+import mkdir         from  "mk-dirs";       // 폴더생성
+import writeFile     from  "write";         // 파일생성
+import gulpCached    from  "gulp-cached";
+import gulpWatch     from  "gulp-watch";    // 실시간 감시
+import gulpScss      from  "gulp-sass";     // sass/scss
+import gulpBabel     from  "gulp-babel";    // js es5변환
+import gulpSync      from  "browser-sync";  // 서버실행, create() 기능첨부되어야함
 const  gulpBrowser = gulpSync.create();
 
 
@@ -30,12 +29,15 @@ async function FnmakeDir(){
 	  mkdir(url.before + 'scss/src'),
 	  mkdir(url.before + 'scss/base'),
 	  mkdir(url.before + 'img'),
+	  mkdir(url.before + 'publicJs/base'),
+		mkdir(url.before + 'publicJs/src'),
 	  mkdir(url.before + 'js/base'),
 	  mkdir(url.before + 'js/src'),
 	  mkdir(url.before + 'media/audio'),
 	  mkdir(url.before + 'media/video'),
 	  mkdir(url.before + 'font'),
-	  mkdir(url.before + 'IE')
+		mkdir(url.before + 'IE'),
+		mkdir(url.dest)
 	])
   }
 
@@ -45,7 +47,7 @@ async function Fnmakefile(){
   <!DOCTYPE html>\n<!-- index.html --><html lang="ko-KR">\n<head>\n  <meta charset="UTF-8">\n  <title>Document</title>\n  <script>\n    window.location = "./html/main.html";\n  </script>\n</head>\n<body>  \n</body>\n</html>`); 
 
 	writeFile.sync(url.before+'html/main.html',`
-	<!DOCTYPE html> \n <!--main.html--> \n <html lang = "ko-KR"> \n <head> \n <meta charset="UTF-8"> \n <meta name="viewport"	content="width=device-width, initial-scale=1.0"> \n <link rel="stylesheet"	href="../font/fontawesome/css/all.css"> \n <link rel="stylesheet"	href="../css/base/normalize.css"> \n <link rel="stylesheet"	href="../css/src/main.css"> \n\ <link rel="shortcut icon"	href="favicon.png" type="image/png"> \n <link rel="apple-touch-icon" href="favicon.png"> \n <title> site name </title>\n</head> \n <body> \n <!--layout--> \n   <div id="wrap"> \n     <header id="headBox"> \n       <h1> <a href="#"> sitename </a></h1> \n       </header>\n    <main id="mainBox">\n      <h2>content</h2> \n     </main>\n    <footer id="footBox">\n      <h2>company infomation</h2> \n     </footer>\n  </div> \n <!--script--> \n <script src="../js/base/jquery.min.js"> </script>\n  <script src="../js/base/jquery-ui.min.js"></script>\n  <script src="../js/src/main.js"></script>\n</body>\n</html>\n`);
+	<!DOCTYPE html> \n <!--main.html--> \n <html lang = "ko-KR"> \n <head> \n <meta charset="UTF-8"> \n <meta name="viewport"	content="width=device-width, initial-scale=1.0"> \n <link rel="stylesheet"	href="../font/fontawesome/css/all.css"> \n <link rel="stylesheet"	href="../css/base/normalize.css"> \n <link rel="stylesheet"	href="../css/src/main.css"> \n\ <link rel="shortcut icon"	href="favicon.png" type="image/png"> \n <link rel="apple-touch-icon" href="favicon.png"> \n <title> site name </title>\n</head> \n <body> \n <!--layout--> \n   <div id="wrap"> \n     <header id="headBox"> \n       <h1> <a href="#"> 사이트가 생성되었습니다. </a></h1> \n       </header>\n    <main id="mainBox">\n      <h2>설치내용</h2> \n  <ul>\n   <li>reset, common css</li>\n   <li>fontawesome</li>\n    <li>jquery</li>\n     <li>scss</li>\n    </ul>\n   </main>\n    <footer id="footBox">\n      <h2>xido school setting</h2> \n     </footer>\n  </div> \n <!--script--> \n <script src="../js/base/jquery.min.js"> </script>\n  <script src="../js/base/jquery-ui.min.js"></script>\n  <script src="../js/src/main.js"></script>\n</body>\n</html>\n`);
 
 	writeFile.sync(url.before+'scss/base/_reset.scss',`
 	@charset "UTF-8";html,body{width:100%; height:100%; margin:0; padding:0; color:#333; font-family:"noto sans", "Gothic A1", sans-serif;}h1,h2,h3,h4,h5,h6, ul,li,ol,dl,dt,dd, p,pre,img{margin: 0; padding: 0; border:0;} ul,ol,li{list-style:none;}table, thead, tbody, tfoot, tr, th, td{ margin:0; padding:0; border-spacing:0; border-collapse:collapse; border:1px solid #777;}          a{color:#777; text-decoration:none;}a:hover{color:#f07;}a:focus{outline:2px solid #f06;}abbr{text-decoration:none; text-transform:uppercase;} button{border:0; cursor:pointer; background-color:transparent; border:1px solid #f06;}button:hover{background-color:#fa0;}button:focus{outline:2px solid #f06;}hr{margin:0; padding:0; border-bottom:1px solid #f06;} main, header, footer, aside, nav, section, article, figure, figcaption{display:block;}form, fieldset, legend{margin:0; padding:0; border:0;}legend{position:absolute; width:0; height:0; overflow:hidden;}input{box-sizing:border-box; text-indent:0.5rem; cursor: pointer;}button{border:0; background-color:#07f; color:#fff; cursor:pointer;}select, text-area{box-sizing:border-box;}input[type="text"]:hover,   input[type="password"]:hover,input[type="search"]:hover, input[type="url"]:hover,input[type="email"]:hover,  input[type="tel"]:hover,text-area:hover, select:hover { background-color: #fdc;}input[type="text"]:focus,   input[type="password"]:focus,input[type="search"]:focus, input[type="url"]:focus,input[type="email"]:focus,  input[type="tel"]:focus,text-area:focus, select:focus { background-color: #fcd;}`);
@@ -55,16 +57,19 @@ async function Fnmakefile(){
 	
 	writeFile.sync(url.before+'scss/base/normalize.scss',`@import "reset"; @import "common";`);
 	
-	writeFile.sync(url.before+'scss/src/main.scss',`@charset "utf-8"; /* main.css */`);
+	writeFile.sync(url.before+'scss/src/main.scss',`
+		@charset "utf-8"; \n	/* main.css */  \n	#wrap{padding-top:5rem;text-align:center}\n	h1{padding:1rem; margin-bottom:2rem;}  \n	#mainBox{width:60%; max-width:600px; min-width:300px; margin:3rem auto;}\n	#mainBox>h2{margin-bottom:0.5rem; color:#07f;}\n	ul{padding:1rem; background-color:#fc0; width:100%; min-width:400px; border-radius:1rem;} \n	li{padding:0.5rem; border-bottom:1px solid #333; margin-bottom:1rem;font-weight: bold;}\n p{padding-top:1rem; font-weight:bold}`);
 
-	writeFile.sync(url.before+'js/src/main.js', `// main.js \n (function($){\n //jquery start \n})(jQuery);`);
+	writeFile.sync(url.before +'publicJs/src/main.js', `
+	// main.js \n (function($){\n //jquery start \n const wrap = $('#wrap'); \n const mainBox = $('#mainBox'); let color = '#ccf'; \n wrap.css({backgroundColor:color}); \n mainBox.append(\`<p>jquery를 이용하여 배경색상을 \$\{color\}로 변경하였습니다.</p>\`); \n})(jQuery);`);
   }
 
 
   /* jquery 불러오기 */
   function FnJquery(){
 	  gulp.src([url.module + 'jquery/dist/jquery.min.js', url.module + 'jqueryui/jquery-ui.min.js', ])
-	      .pipe(gulp.dest(url.before+'js/base/'));
+			.pipe(gulp.dest(url.before +'publicJs/base/'))
+	    .pipe(gulp.dest(url.before+'js/base/'));
 	}
 
 	/* fontawesome 불러오기(fortawesome 으로 이름에 오타가 있음) */
@@ -104,10 +109,13 @@ function FnHtml(){
 		.pipe(gulpBrowser.reload({stream:true}));
 }
 
-function FnJs(){
-	gulp.src(url.before+'js/**/*.js')
+async function FnJs(){
+	gulp.src(url.before+'publicJs/**/*.js')
 		.pipe(gulpCached('jsFiles'))
-		.pipe(babel())
+		.pipe(gulpBabel({
+			presets:['@babel/env']
+		}))
+		.pipe(gulp.dest(url.before+'js/'))
 		.pipe(gulpBrowser.reload({stream:true}));
 }
 
@@ -129,16 +137,17 @@ function FnWatch(){
 	gulpWatch(url.before+'**/*.html', FnHtml);
 	gulpWatch(url.before+'scss/**/*.scss', FnScss);
 	gulpWatch(url.before+'css/**/*.css', FnCss);
-	gulpWatch(url.before+'js/**/*.js', FnJs);
+	gulpWatch(url.before+'publicJs/**/*.js', FnJs);
+	// gulpWatch(url.before+'js/**/*.js', FnJs);
 }
 // ================================
 /* 삭제하기 */
 function FnAlldel(){
-	return filedel(['./public/']);
+ gulp.src('./public', {read:false}).pipe(gulpClean());
 };
 
 function Fndel(){
-	return filedel(['./public/dest/**', '!public']);
+	gulp.src('./public/dest/**', { read: false }).pipe(gulpClean());	gulpClean();
 }
 
 // ================================
@@ -147,10 +156,10 @@ export const makedir = FnmakeDir;
 export const makefile = gulp.series(FnmakeDir, gulp.parallel(Fnmakefile, FnJquery, FnFontAwesome));
 export const scss = FnScss; //gulp scss
 export const server = FnSync; //gulp server
-export const clean = FnAlldel();
-export const destdell = Fndel();
+// export const clean = FnAlldel();
+// export const destdell = Fndel();
 
 
-const myWeb = gulp.series(FnScss, gulp.parallel(FnSync, FnWatch));
+const myWeb = gulp.series(FnScss, FnJs, gulp.parallel(FnSync, FnWatch));
 export default myWeb;
 
